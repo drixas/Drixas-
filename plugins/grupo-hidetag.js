8bc3f2fef5400ab013ca15cad4a6b638fdd364b2
drixas <brayanchirinos.2005@gmail.com>
2025-05-21 20:56:48 -0500

    Actualizar grupo-hidetag.js

--- plugins/grupo-hidetag.js
+++ plugins/grupo-hidetag.js
@@ -1,49 +1,74 @@
-//code traído por Xi_Crew
+// code traído por Xi_Crew
 import { generateWAMessageFromContent } from '@whiskeysockets/baileys'
 import * as fs from 'fs'
 
 var handler = async (m, { conn, text, participants, isOwner, isAdmin }) => {
+  const firma = '𝘋𝘳𝘪𝘹𝘢𝘴-𝘉𝘰𝘵'
+  const salto = '\n\n' // dos líneas
 
-if (!m.quoted && !text) return conn.reply(m.chat, `${emoji} Debes enviar un texto para hacer un tag.`, m)
+  if (!m.quoted && !text) return conn.reply(m.chat, `Debes enviar un texto para hacer un tag.`, m)
 
-try { 
+  try {
+    let users = participants.map(u => conn.decodeJid(u.id))
+    let q = m.quoted ? m.quoted : m || m.text || m.sender
+    let c = m.quoted ? await m.getQuotedObj() : m.msg || m.text || m.sender
+    let msg = conn.cMod(
+      m.chat,
+      generateWAMessageFromContent(
+        m.chat,
+        {
+          [m.quoted ? q.mtype : 'extendedTextMessage']:
+            m.quoted ? c.message[q.mtype] : { text: '' || c }
+        },
+        { quoted: null, userJid: conn.user.id }
+      ),
+      (text || q.text) + salto + firma,
+      conn.user.jid,
+      { mentions: users }
+    )
+    await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
 
-let users = participants.map(u => conn.decodeJid(u.id))
-let q = m.quoted ? m.quoted : m || m.text || m.sender
-let c = m.quoted ? await m.getQuotedObj() : m.msg || m.text || m.sender
-let msg = conn.cMod(m.chat, generateWAMessageFromContent(m.chat, { [m.quoted ? q.mtype : 'extendedTextMessage']: m.quoted ? c.message[q.mtype] : { text: '' || c }}, { quoted: null, userJid: conn.user.id }), text || q.text, conn.user.jid, { mentions: users })
-await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
+  } catch {
 
-} catch {  
+    let users = participants.map(u => conn.decodeJid(u.id))
+    let quoted = m.quoted ? m.quoted : m
+    let mime = (quoted.msg || quoted).mimetype || ''
+    let isMedia = /image|video|sticker|audio/.test(mime)
+    let more = String.fromCharCode(8206)
+    let masss = more.repeat(850)
 
-/**
-[ By @NeKosmic || https://github.com/NeKosmic/ ]
-**/  
+    let htextos = `${text ? text : "*Hola!!*"}${salto}${firma}`
 
-let users = participants.map(u => conn.decodeJid(u.id))
-let quoted = m.quoted ? m.quoted : m
-let mime = (quoted.msg || quoted).mimetype || ''
-let isMedia = /image|video|sticker|audio/.test(mime)
-let more = String.fromCharCode(8206)
-let masss = more.repeat(850)
-let htextos = `${text ? text : "*Hola!!*"}`
-if ((isMedia && quoted.mtype === 'imageMessage') && htextos) {
-var mediax = await quoted.download?.()
-conn.sendMessage(m.chat, { image: mediax, mentions: users, caption: htextos, mentions: users }, { quoted: null })
-} else if ((isMedia && quoted.mtype === 'videoMessage') && htextos) {
-var mediax = await quoted.download?.()
-conn.sendMessage(m.chat, { video: mediax, mentions: users, mimetype: 'video/mp4', caption: htextos }, { quoted: null })
-} else if ((isMedia && quoted.mtype === 'audioMessage') && htextos) {
-var mediax = await quoted.download?.()
-conn.sendMessage(m.chat, { audio: mediax, mentions: users, mimetype: 'audio/mp4', fileName: `Hidetag.mp3` }, { quoted: null })
-} else if ((isMedia && quoted.mtype === 'stickerMessage') && htextos) {
-var mediax = await quoted.download?.()
-conn.sendMessage(m.chat, {sticker: mediax, mentions: users}, { quoted: null })
-} else {
-await conn.relayMessage(m.chat, {extendedTextMessage:{text: `${masss}\n${htextos}\n`, ...{ contextInfo: { mentionedJid: users, externalAdReply: { thumbnail: icons, sourceUrl: redes }}}}}, {})
-}}
-
+    if ((isMedia && quoted.mtype === 'imageMessage') && htextos) {
+      var mediax = await quoted.download?.()
+      conn.sendMessage(m.chat, { image: mediax, mentions: users, caption: htextos }, { quoted: null })
+    } else if ((isMedia && quoted.mtype === 'videoMessage') && htextos) {
+      var mediax = await quoted.download?.()
+      conn.sendMessage(m.chat, { video: mediax, mentions: users, mimetype: 'video/mp4', caption: htextos }, { quoted: null })
+    } else if ((isMedia && quoted.mtype === 'audioMessage') && htextos) {
+      var mediax = await quoted.download?.()
+      conn.sendMessage(m.chat, { audio: mediax, mentions: users, mimetype: 'audio/mp4', fileName: `Hidetag.mp3` }, { quoted: null })
+    } else if ((isMedia && quoted.mtype === 'stickerMessage') && htextos) {
+      var mediax = await quoted.download?.()
+      conn.sendMessage(m.chat, { sticker: mediax, mentions: users }, { quoted: null })
+    } else {
+      await conn.relayMessage(
+        m.chat,
+        {
+          extendedTextMessage: {
+            text: `${masss}\n${htextos}\n`,
+            contextInfo: {
+              mentionedJid: users
+              // Puedes añadir aquí externalAdReply si deseas
+            }
+          }
+        },
+        {}
+      )
+    }
+  }
 }
+
 handler.help = ['hidetag']
 handler.tags = ['grupo']
 handler.command = ['hidetag', 'notificar', 'notify', 'tag']
