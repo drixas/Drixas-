6ce81a220bec130eb4b7b4362c2d2ebbc29c7670
drixas <brayanchirinos.2005@gmail.com>
2025-05-27 01:58:41 -0500

    Actualizar grupo-config.js

--- plugins/grupo-config.js
+++ plugins/grupo-config.js
@@ -1,28 +1,44 @@
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => icono) 
-let isClose = { // Switch Case Like :v
-'open': 'not_announcement',
-'close': 'announcement',
-'abierto': 'not_announcement',
-'cerrado': 'announcement',
-'abrir': 'not_announcement',
-'cerrar': 'announcement',
-}[(args[0] || '')]
-if (isClose === undefined)
-return conn.reply(m.chat, `${emoji} *Elija una opción para configurar el grupo*\n\nEjemplo:\n*✰ #${command} abrir*\n*✰ #${command} cerrar*\n*✰ #${command} close*\n*✰ #${command} open*`, m)
-await conn.groupSettingUpdate(m.chat, isClose)
+  const emoji = '🔓';
+  const emoji2 = '🔒';
+  const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1745522645448.jpeg');
 
-if (isClose === 'not_announcement'){
-m.reply(`${emoji} *Ya pueden escribir en este grupo.*`)
-}
+  let isClose = {
+    'open': 'not_announcement',
+    'close': 'announcement',
+    'abierto': 'not_announcement',
+    'cerrado': 'announcement',
+    'abrir': 'not_announcement',
+    'cerrar': 'announcement',
+  }[(args[0] || '').toLowerCase()];
 
-if (isClose === 'announcement'){
-m.reply(`${emoji2} *Solos los admins pueden escribir en este grupo.*`)
-}}
-handler.help = ['group open / close', 'grupo abrir / cerrar']
-handler.tags = ['grupo']
-handler.command = ['group', 'grupo']
-handler.admin = true
-handler.botAdmin = true
+  if (isClose === undefined) {
+    return conn.reply(
+      m.chat,
+      `❗ 𝐄𝐥𝐢𝐠𝐞 𝐮𝐧𝐚 𝐨𝐩𝐜𝐢𝐨́𝐧 𝐩𝐚𝐫𝐚 𝐜𝐨𝐧𝐟𝐢𝐠𝐮𝐫𝐚𝐫 𝐞𝐥 𝐠𝐫𝐮𝐩𝐨:\n\n` +
+      `✰ ${emoji} *#${command} 𝐚𝐛𝐫𝐢𝐫*\n` +
+      `✰ ${emoji2} *#${command} 𝐜𝐞𝐫𝐫𝐚𝐫*\n` +
+      `✰ ${emoji} *#${command} 𝐨𝐩𝐞𝐧*\n` +
+      `✰ ${emoji2} *#${command} 𝐜𝐥𝐨𝐬𝐞*`,
+      m
+    );
+  }
+
+  await conn.groupSettingUpdate(m.chat, isClose);
+
+  if (isClose === 'not_announcement') {
+    m.reply(`🔓 𝐄𝐥 𝐠𝐫𝐮𝐩𝐨 𝐞𝐬𝐭𝐚́ 𝐀𝐁𝐈𝐄𝐑𝐓𝐎, 𝐭𝐨𝐝𝐨𝐬 𝐩𝐮𝐞𝐝𝐞𝐧 𝐞𝐬𝐜𝐫𝐢𝐛𝐢𝐫.`);
+  }
+
+  if (isClose === 'announcement') {
+    m.reply(`🔒 𝐄𝐥 𝐠𝐫𝐮𝐩𝐨 𝐡𝐚 𝐬𝐢𝐝𝐨 𝐂𝐄𝐑𝐑𝐀𝐃𝐎, 𝐬𝐨𝐥𝐨 𝐚𝐝𝐦𝐢𝐧𝐬 𝐩𝐮𝐞𝐝𝐞𝐧 𝐞𝐬𝐜𝐫𝐢𝐛𝐢𝐫.`);
+  }
+};
+
+handler.help = ['group open / close', 'grupo abrir / cerrar'];
+handler.tags = ['grupo'];
+handler.command = ['group', 'grupo'];
+handler.admin = true;
+handler.botAdmin = true;
 
 export default handler
