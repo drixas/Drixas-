a34e562e4ca3b468a5c46c0c2778c5b00591fd77
Neykoor💜 <chinquepapa@gmail.com>
2025-05-28 22:53:05 -0700

    Update main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -65,10 +65,10 @@ global.waitt = '❍ Espera un momento, soy lenta...';
 global.waittt = '❍ Espera un momento, soy lenta...';
 global.waitttt = '❍ Espera un momento, soy lenta...';
 
-var canal = 'https://whatsapp.com/channel/0029VbAfPu9BqbrEMFWXKE0d'  
+var canal = 'https://whatsapp.com/channel/0029VbAqmLQHrDZm0YXgqB3r'  
 var comunidad = 'https://chat.whatsapp.com/I0dMp2fEle7L6RaWBmwlAa'
-var git = 'https://github.com/The-King-Destroy'
-var github = 'https://github.com/The-King-Destroy/Yuki_Suou-Bot' 
+var git = 'https://github.com/Drixas--drixas'
+var github = 'https://github.com/drixas/Drixas-' 
 let correo = 'thekingdestroy507@gmail.com'
 global.redes = [canal, comunidad, git, github, correo].getRandom()
 
