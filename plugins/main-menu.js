ce070f5141df6871eca3c0605adcf393c8774917
Neykoor💜 <chinquepapa@gmail.com>
2025-05-29 18:46:07 -0700

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -25,17 +25,51 @@ let handler = async (m, { conn, args }) => {
 │⏰ Activa: ${uptime}
 │🌸 Comunidad: ${totalreg}
 │🧩 Comandos: ${totalCommands}
-│🔗 Conexión: Baileys
+│🔗 \`Conexión:\` Baileys
 ╰───────────────♡
 
-╭─♡︰𝐈𝐧𝐟𝐨-𝐁𝐨𝐭
-│ 🎀 \`#uptime\` / \`#runtime\`
-│ ↳ Saber mi tiempo activa ⏳
-│ 🎀 \`#sc\` / \`#script\`
-│ ↳ Mi código fuente 💻
-│ 🎀 \`#ping\` / \`#p\`
-│ ↳ Velocidad de respuesta ⚡
-╰───────────────♡
+  ♡︰𝐈𝐧𝐟𝐨-𝐁𝐨𝐭
+❍ Comandos para ver estado e información de la Bot.
+ᰔᩚ *#help • #menu*
+> ✦ Ver la lista de comandos de la Bot.
+ᰔᩚ *#uptime • #runtime*
+> ✦ Ver tiempo activo o en linea de la Bot.
+ᰔᩚ *#sc • #script*
+> ✦ Link del repositorio oficial de la Bot
+ᰔᩚ *#staff • #colaboradores*
+> ✦ Ver la lista de desarrolladores de la Bot.
+ᰔᩚ *#serbot • #serbot code*
+> ✦ Crea una sesión de Sub-Bot.
+ᰔᩚ *#bots • #sockets*
+> ✦ Ver la lista de Sub-Bots activos.
+ᰔᩚ *#creador*
+> ✦ Contacto del creador de la Bot.
+ᰔᩚ *#status • #estado*
+> ✦ Ver el estado actual de la Bot.
+ᰔᩚ *#links • #grupos*
+> ✦ Ver los enlaces oficiales de la Bot.
+ᰔᩚ *#infobot • #infobot*
+> ✦ Ver la información completa de la Bot.
+ᰔᩚ *#sug • #newcommand*
+> ✦ Sugiere un nuevo comando.
+ᰔᩚ *#p • #ping*
+> ✦ Ver la velocidad de respuesta del Bot.
+ᰔᩚ *#reporte • #reportar*
+> ✦ Reporta alguna falla o problema de la Bot.
+ᰔᩚ *#sistema • #system*
+> ✦ Ver estado del sistema de alojamiento.
+ᰔᩚ *#speed • #speedtest*
+> ✦ Ver las estadísticas de velocidad de la Bot.
+ᰔᩚ *#views • #usuarios*
+> ✦ Ver la cantidad de usuarios registrados en el sistema.
+ᰔᩚ *#funciones • #totalfunciones*
+> ✦ Ver todas las funciones de la Bot.
+ᰔᩚ *#ds • #fixmsgespera*
+> ✦ Eliminar archivos de sesión innecesarios.
+ᰔᩚ *#editautoresponder*
+> ✦ Configurar un Prompt personalizado de la Bot.
+
+• :･ﾟ⊹˚• \`『 Buscadores 』\` •˚⊹:･ﾟ•
 `.trim();
 
   await conn.sendMessage(m.chat, {
