83461cd97b8ac58136f933b5f7ed1c38fcd12cb0
Neykoor💜 <chinquepapa@gmail.com>
2025-06-04 01:19:38 -0700

    Update main-script.js

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -1,33 +1,10 @@
-import moment from 'moment-timezone'
-import fetch from 'node-fetch'
+const handler = async (m, { conn }) => {
+  await conn.sendMessage(m.chat, {
+    text: '> Este es un bot privado y por el momento no se ha liberado para el público.'
+  }, { quoted: m });
+};
 
-let handler = async (m, { conn, args }) => {
-try {
-let res = await fetch('https://api.github.com/repos/The-King-Destroy/Yuki_Suou-Bot')
+handler.command = handler.help = ['sc', 'script'];
+handler.tags = ['info'];
 
-if (!res.ok) throw new Error('Error al obtener datos del repositorio')
-let json = await res.json()
-
-let txt = `*乂  S C R I P T  -  M A I N  乂*\n\n`
-txt += `✩  *Nombre* : ${json.name}\n`
-txt += `✩  *Visitas* : ${json.watchers_count}\n`
-txt += `✩  *Peso* : ${(json.size / 1024).toFixed(2)} MB\n`
-txt += `✩  *Actualizado* : ${moment(json.updated_at).format('DD/MM/YY - HH:mm:ss')}\n`
-txt += `✩  *Url* : ${json.html_url}\n`
-txt += `✩  *Forks* : ${json.forks_count}\n`
-txt += `✩  *Stars* : ${json.stargazers_count}\n\n`
-txt += `> *${dev}*`
-
-await conn.sendMessage(m.chat, {text: txt, contextInfo: { forwardingScore: 999, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterName: channelRD.name, newsletterJid: channelRD.id, }, externalAdReply: { title: packname, body: dev, thumbnailUrl: 'https://qu.ax/nGaLj.jpg', sourceUrl: redes, mediaType: 1, renderLargerThumbnail: true }}}, {quoted: fkontak})
-
-} catch {
-await conn.reply(m.chat, `${msm} Ocurrió un error.`, m)
-await m.react(error)
-}}
-
-handler.help = ['script']
-handler.tags = ['main']
-handler.command = ['script', 'sc']
-handler.register = true
-
-export default handler
+export default handler;
