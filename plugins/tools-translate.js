bf0828f35d096437b371da1907d71a0415029c62
https_(S2) <johanstil028@gmail.com>
2025-07-09 20:13:06 -0500

    Update tools-translate.js

--- plugins/tools-translate.js
+++ plugins/tools-translate.js
@@ -1,7 +1,14 @@
 import translate from '@vitalets/google-translate-api';
 import fetch from 'node-fetch';
+
+
 const handler = async (m, {args, usedPrefix, command}) => {
-  const msg = `${emoji} Por favor, ingresé el (idioma) (texto) para traducirlo.`;
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
+  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
+  const tradutor = _translate.plugins.herramientas__translate
+
+  const msg = `${tradutor.texto1[0]} ${usedPrefix + command} ${tradutor.texto1[1]}\n*${usedPrefix + command} ${tradutor.texto1[2]}\n*- https://cloud.google.com/translate/docs/languages*`;
   if (!args || !args[0]) return m.reply(msg);
   let lang = args[0];
   let text = args.slice(1).join(' ');
@@ -12,21 +19,18 @@ const handler = async (m, {args, usedPrefix, command}) => {
   }
   if (!text && m.quoted && m.quoted.text) text = m.quoted.text;
   try {
-   const result = await translate(`${text}`, {to: lang, autoCorrect: true});
-    await conn.reply(m.chat, result.text, m);
+    const result = await translate(`${text}`, {to: lang, autoCorrect: true});
+    await m.reply(tradutor.texto3 + result.text);
   } catch {
     try {
-    conn.reply(m.chat, wait, m, {
-    contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true }}})
       const lol = await fetch(`https://api.lolhuman.xyz/api/translate/auto/${lang}?apikey=${lolkeysapi}&text=${text}`);
       const loll = await lol.json();
       const result2 = loll.result.translated;
-      await conn.reply(m.chat, result2, m);
+      await m.reply(`${tradutor.texto3 }` + result2);
     } catch {
-      await m.reply(`${msm} Ocurrió un error.`);
+      await m.reply(tradutor.texto2);
     }
   }
 };
-handler.command = ['translate','traducir','trad'];
-handler.register = true
+handler.command = /^(translate|traducir|trad)$/i;
 export default handler;
